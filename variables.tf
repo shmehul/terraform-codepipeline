e5641cd9f590e7d9e3a429be79f741ebe1349baa
shmehul <shmehul@users.noreply.github.com>
2020-08-04 13:09:44 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "account_name" {
+  default = "meka"
+}
+
+variable "github_org" {
+  default = "meka"
+}
+
+variable "github_token" {
+}
+
+variable "project" {
+	default = "terraform-codepipeline-example"
+}
+
+variable "app" {
+	default = "terraform-codepipeline-example"
+}
+
+variable "account_id" {
+}
+
+variable "docker_build_image" {
+  default = "ubuntu"
+}
